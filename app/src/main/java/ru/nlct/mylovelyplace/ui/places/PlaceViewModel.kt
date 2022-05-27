edebac69be2e84932d2172698044a479b660d8a0
Eldar Tatarkanov <eldar.tatarkanov@rt.ru>
2022-05-27 23:57:05 +0300

    Initial Commit

---
+++ app/src/main/java/ru/nlct/mylovelyplace/ui/places/PlaceViewModel.kt
@@ -0,0 +1,13 @@
+package ru.nlct.mylovelyplace.ui.places
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class PlaceViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is place Fragment"
+    }
+    val text: LiveData<String> = _text
+}
